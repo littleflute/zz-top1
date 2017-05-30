fd6e08809acdb164bdac4087431589b2a0b3a01a
littleflute <13699175164@sina.cn>
2017-05-30 15:18:04 -0600

    Add files via upload

---
+++ Four Flat Tires on a Muddy Road- All Star Tribute to ZZ Top/mj.bat
@@ -0,0 +1,17 @@
+del m.js 
+
+echo var f = [];				>>m.js
+echo f[0]	= "v0.0.7";			>>m.js
+
+setlocal ENABLEDELAYEDEXPANSION
+call set /a x = 0
+
+for /F "delims=" %%a in ('dir /ad /b') do (
+	call set /a x = !x! + 1
+	echo f[!x!]	= "%%a";		>>m.js
+)
+
+for /F "delims=" %%a in ('dir  /b *mp3') do (
+	call set /a x = !x! + 1
+	echo f[!x!]	= "%%a";		>>m.js
+)
